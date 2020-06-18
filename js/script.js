52c7dc4cbec54a524f57ae8553d1849b2794c2ef
Laura <lstorella10@gmail.com>
2020-06-18 12:17:50 +0200

    modifica js - funzione visualizza chat contatto

--- js/script.js
+++ js/script.js
@@ -17,6 +17,14 @@ Step #4 - Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono vi
 */
 
 
+/*
+MILESTONE 3
+Step #5 -Click sul contatto mostra la conversazione del contatto cliccato, è possibile inserire nuovi messaggi per ogni conversazione
+
+Step #6 - Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+*/
+
+
 
 
 
@@ -35,7 +43,7 @@ Step #4 - Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono vi
 //e cliccando “invia” il testo viene aggiunto al thread sopra,
 //come messaggio verde
 
-// // Creo evento click tramite tasto invio nella barra di ricerca
+// Creo evento click tramite tasto invio nella barra di ricerca
 $('.send-input').click( function() {
   sendMsg();
 });
@@ -105,21 +113,6 @@ function pcReplay() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // -------------------- Funzione ricerca contatto in lista ------------------
 // Alla pressione di un tasto nel campo barra di ricerca
 $('.search-bar').keyup(function(event){
@@ -145,24 +138,92 @@ $('.search-bar').keyup(function(event){
 
 
 
+// MILESTONE 3
+// -------------------- Funzione mostra chat contatto cliccato --------------
 
+// Creo evento click su contatto nella lista
+$('.contact.box').click( function() {
 
+  // var dataContact = $(this).attr('data-contact');
+  // console.log(dataContact);  // ritorna n° data-contact corrispondente
 
+  // Utente clicca su uno dei contatti in elenco,
+  // in sezione chat dx appare la relativa conversazione associata (l'attr data-contact e data-chat collega il contatto con la sua chat)
 
+  // Rimuovo la classe .active a elemento .contact.box corrente
+  //     ---> di default a 1° elemento .contact.box (rende sfondo più scuro)
+  $('.contact.box').removeClass('active');
+  // Aggiungo la classe .active al contatto su cui ho cliccato
+  $(this).addClass('active');
 
+  // Prendo il valore di data-chat relativo al contatto su cui ho cliccato
+  var dataChat = $(this).attr('data-chat');
 
+  // Rimuovo la classe .chat-active a tutti gli elementi .chat per nascondere le chat
+    //     ---> di default a 1° elemento .chat
+  $('.chat').removeClass('chat-active');
 
+  // Prendo l'elemento div contenente il msg  il cui attr data-chat corrisponde a quello del contatto cliccato e assegno classe .chat-active
+  var generatedMsg = $('.chat [data-chat = " ' + dataChat + ' "]').addClass('chat-active');
 
+  // Prendo il nome del contatto su cui ho cliccato
+  var contactName = $(this).find('.contact-name').text();
 
+  // Visualizzo  il nome del contatto in header dx
+  $('.myprofile-right-header .user-name').text(contactName);
 
+  // Prendo percorso file dell'img profilo del contatto su cui ho cliccato
+  var contactImgPath = $(this).find('.icon-box img').attr('src');
 
-
-
-
-
-
-
-
-
-
+  // Visualizzo img profilo del contatto cliccato in header dx tramite path
+  $('.myprofile-right-header img').attr('src', contactImgPath);
 });
+
+
+
+
+
+// // -------------------- Funzione cancella msg chat --------------------
+//
+// // Creo evento click sull'icona del dropdown del msg
+// $('.msg-wrapper').mouseenter( function() {
+//
+//
+//
+//   $(this).children('.hide span').removeClass('hide');
+//   // Prendo il div .message-options relativo al msg su cui ho cliccato e aggiungo la classe .active per rendere visibile la finestra opzioni msg
+//   // $(this).siblings('.message-options').toggleClass('active');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});  // document ready
